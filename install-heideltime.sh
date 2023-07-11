af47b7c8d69eb924825a1ee48154ab2c5a067f90
dehisre01 <120452103+dehisre01@users.noreply.github.com>
2023-07-11 13:19:25 +0200

    Update install-heideltime.sh

--- install-heideltime.sh
+++ install-heideltime.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
-# install other requirements
-pip install -r requirements.txt
-
-mkdir data
+# you need wheel to install heideltime
+pip install wheel
 
 # install python_heideltime
 cd python_heideltime
@@ -12,3 +10,8 @@ chmod +x install_heideltime_standalone.sh
 
 python3 -m pip install .
 
+# install other requirements
+cd ..
+pip install -r requirements.txt
+
+mkdir data
